6640b1a3dd20e1f3ada852c6aae384764c78bc31
chabazite <ingallsandrew0@gmail.com>
2022-08-15 15:06:03 -0500

    changed gunicorn

---
+++ web_app/app.py
@@ -0,0 +1,5 @@
+from dashboard_app import app
+
+    #app.run(host='0.0.0.0', port=3000, debug=True) 
+
+app.run(host='0.0.0.0', port=8080)
--- web_app/dashboard_app.py
+++
@@ -1,3 +0,0 @@
-from dashboard_app import app
-
-    #app.run(host='0.0.0.0', port=3000, debug=True) 
